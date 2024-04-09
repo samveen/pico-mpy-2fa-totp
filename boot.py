80cb8a3827b45c83aa4c8ed9d03906347d710762
Samveen <samveen@samveen.in>
2024-04-09 11:13:38 +0530

    Optimizations to boot.py for reduced memory usage
    Signed-off-by: Samveen <samveen@samveen.in>

--- boot.py
+++ boot.py
@@ -1,32 +1,38 @@
 import machine
+import ustruct as struct
+
+# Common RTC
+r=None
+rtsaver=None
 
 # function to save the RTC
 def rtSaverCallback(t):
-    from ustruct import pack
-    r=machine.RTC()
-    with open("hwclock.save","w") as fw: fw.write(pack("<8I",*(r.datetime())))
+    global r
+    with open("hwclock.save","w") as fw: fw.write(struct.pack("<8I",*(r.datetime())))
 
 # Load the saved RTC time, and start the rtc saver save timer
 def fakehwclock():
-    from ustruct import unpack
-    r=machine.RTC()
+    global r
+    global rtsaver
     # Restore RTC save
     try:
-        with open("hwclock.save","rb") as fr: r.datetime(unpack("<8I",fr.read()))
+        with open("hwclock.save","rb") as fr: r.datetime(struct.unpack("<8I",fr.read()))
     except OSError as e:
-        r.datetime((2023, 1, 29, 6, 13, 55, 0, 0))
+        r.datetime((2024, 4, 9, 6, 04, 55, 0, 0))
     except ValueError as e:
-        r.datetime((2023, 2, 9, 4, 19, 8, 0, 0))
+        r.datetime((2024, 4, 9, 6, 04, 55, 0, 0))
 
     # periodic firing of the function every 5000ms
-    rtsaver = machine.Timer(mode=machine.Timer.PERIODIC, period=5000, callback=rtSaverCallback)
-
+    if rtsaver == None:
+        rtsaver = machine.Timer(mode=machine.Timer.PERIODIC, period=5000, callback=rtSaverCallback)
 
 def board_init():
+    global r
     # Turn on the LED
     machine.Pin("LED", machine.Pin.OUT).value(1)
+    # Init the common RTC
+    r=machine.RTC()
 
     fakehwclock()
-    r=machine.RTC()
 
 board_init()
